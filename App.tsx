1aa150882a3f4f190a9620dcfdfe0870b728b7d9
Charlie <chafincharlie11@gmail.com>
2025-06-12 23:19:12 -0500

    fix errors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import TableView from './TableView';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <TableView />
---
+++ TableView.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+declare const TableView: () => React.JSX.Element;
+export default TableView;
